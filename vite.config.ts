0c02f02ee5c68594467e4d9364c7a3b2ffb2ecbe
kotarou1192 <kousuke.ida.nagasaki@gmail.com>
2026-03-15 17:21:41 +0900

    force seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +11,12 @@ export default defineConfig({
   base: "/kishitu-gensen/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        en: resolve(__dirname, "en/index.html"),
+      },
+    },
   },
   test: {
     globals: true,
