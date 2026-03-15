86e7d8ff891dfbc201d9cae09c27480a076efe38
kotarou1192 <kousuke.ida.nagasaki@gmail.com>
2026-03-15 17:06:04 +0900

    update configure

--- eslint.config.js
+++ eslint.config.js
@@ -33,6 +33,7 @@ export default tseslint.config(
   {
     rules: {
       "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
